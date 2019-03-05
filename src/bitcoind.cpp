c91999eb93ac61162954ffed3c0a2ba5d26e6d24
lambo777 <40826434+lambo777@users.noreply.github.com>
2019-03-05 06:26:24 +0000

    Update bitcoind.cpp

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -72,7 +72,7 @@ bool AppInit(int argc, char* argv[])
     // Process help and version before taking care about datadir
     if (mapArgs.count("-?") || mapArgs.count("-h") ||  mapArgs.count("-help") || mapArgs.count("-version"))
     {
-        std::string strUsage = _("ZClassic Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n" + PrivacyInfo();
+        std::string strUsage = _("Bazcoin Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n" + PrivacyInfo();
 
         if (mapArgs.count("-version"))
         {
@@ -81,7 +81,7 @@ bool AppInit(int argc, char* argv[])
         else
         {
             strUsage += "\n" + _("Usage:") + "\n" +
-                  "  zclassicd [options]                     " + _("Start ZClassic Daemon") + "\n";
+                  "  bazcoind [options]                     " + _("Start Bazcoin Daemon") + "\n";
 
             strUsage += "\n" + HelpMessage(HMM_BITCOIND);
         }
@@ -102,21 +102,21 @@ bool AppInit(int argc, char* argv[])
             ReadConfigFile(mapArgs, mapMultiArgs);
         } catch (const missing_zcash_conf& e) {
             fprintf(stderr,
-                (_("Before starting zclassicd, you need to create a configuration file:\n"
+                (_("Before starting bazcoind, you need to create a configuration file:\n"
                    "%s\n"
                    "It can be completely empty! That indicates you are happy with the default\n"
-                   "configuration of zclassicd. But requiring a configuration file to start ensures\n"
-                   "that zclassicd won't accidentally compromise your privacy if there was a default\n"
+                   "configuration of bazcoind. But requiring a configuration file to start ensures\n"
+                   "that bazcoind won't accidentally compromise your privacy if there was a default\n"
                    "option you needed to change.\n"
                    "\n"
                    "You can look at the example configuration file for suggestions of default\n"
                    "options that you may want to change. It should be in one of these locations,\n"
-                   "depending on how you installed ZClassic:\n") +
+                   "depending on how you installed Bazcoin:\n") +
                  _("- Source code:  %s\n"
                    "- .deb package: %s\n")).c_str(),
                 GetConfigFile().string().c_str(),
-                "contrib/debian/examples/zclassic.conf",
-                "/usr/share/doc/zcash/examples/zclassic.conf");
+                "contrib/debian/examples/bazcoin.conf",
+                "/usr/share/doc/zcash/examples/bazcoin.conf");
             return false;
         } catch (const std::exception& e) {
             fprintf(stderr,"Error reading configuration file: %s\n", e.what());
@@ -136,14 +136,14 @@ bool AppInit(int argc, char* argv[])
 
         if (fCommandLine)
         {
-            fprintf(stderr, "Error: There is no RPC client functionality in zclassicd. Use the zclassic-cli utility instead.\n");
+            fprintf(stderr, "Error: There is no RPC client functionality in bazcoind. Use the bazcoin-cli utility instead.\n");
             exit(EXIT_FAILURE);
         }
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon)
         {
-            fprintf(stdout, "ZClassic server starting\n");
+            fprintf(stdout, "Bazcoin server starting\n");
 
             // Daemonize
             pid_t pid = fork();
